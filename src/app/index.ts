9d6d49e43c23ba72577d4b9c4223136f12ab700a
Sthefano Politino <stouma@slingr.io>
2024-09-06 13:34:05 -0300

    fix in file tag

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,3 +3,4 @@ import "./slingr-link/slingr-link";
 import "./slingr-button/slingr-button";
 import "./slingr-action/slingr-action";
 import "./slingr-tooltip/slingr-tooltip";
+import "./slingr-file/slingr-file";
